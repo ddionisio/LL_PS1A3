d4990eb8335da74c03c61b4833937c306b08359f
ddionisio <garapata@gmail.com>
2017-11-21 23:49:19 -0800

    stage 4-2

---
+++ Assets/Scripts/Forces/HeatGODeactivate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeatGODeactivate : MonoBehaviour {
+
+    private HeatController mHeatCtrl;
+    
+    void OnDestroy() {
+        if(mHeatCtrl) {
+            mHeatCtrl.amountChangedCallback -= OnHeatAmountUpdate;
+        }
+    }
+
+    void Awake() {
+        mHeatCtrl = GetComponent<HeatController>();
+        mHeatCtrl.amountChangedCallback += OnHeatAmountUpdate;
+    }
+
+    void OnHeatAmountUpdate(HeatController heat, float prevAmt) {
+        if(heat.amountCurrent >= heat.amountCapacity) {
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/Forces/TriggerToggle.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class TriggerToggle : MonoBehaviour {
+    public bool stayToggled;
+
+    public bool isToggled {
+        get { return mIsToggled; }
+
+        private set {
+            if(mIsToggled != value) {
+                mIsToggled = value;
+                ToggleChanged();
+            }
+        }
+    }
+
+    private bool mIsToggled;
+
+    protected abstract void ToggleChanged();
+
+    void OnTriggerEnter2D(Collider2D collision) {
+        isToggled = true;
+    }
+
+    void OnTriggerExit2D(Collider2D collision) {
+        if(!stayToggled)
+            isToggled = false;
+    }
+}

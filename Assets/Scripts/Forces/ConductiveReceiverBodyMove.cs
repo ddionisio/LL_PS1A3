5ef4afea039c5693ebc80ad38ae5ff1529ee4317
ddionisio <garapata@gmail.com>
2017-11-22 15:34:44 -0800

    some more level tweaks, stage 5-2

---
+++ Assets/Scripts/Forces/ConductiveReceiverBodyMove.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using DG.Tweening.Core.Easing;
+using DG.Tweening;
+
+public class ConductiveReceiverBodyMove : ConductiveReceiver {
+    public Rigidbody2D targetBody;
+
+    public Vector2 endPosition;
+    public bool endIsLocal;
+
+    public Ease tweenStyle;
+    public float startDelay = 0.3f;
+    public float delay = 1.5f;
+
+    private Coroutine mRout;
+    private float mCurTime;
+
+    private Vector2 mStartPosition;
+    private Vector2 mEndPosition;
+
+    void OnDisable() {
+        mRout = null;
+    }
+
+    protected override void Awake() {
+        base.Awake();
+
+        mStartPosition = targetBody.position;
+
+        mEndPosition = endIsLocal ? (Vector2)targetBody.transform.localToWorldMatrix.MultiplyPoint3x4(endPosition) : endPosition;
+    }
+
+    protected override void OnPowerChanged() {
+        if(mRout != null)
+            StopCoroutine(mRout);
+
+        StartCoroutine(DoMove());
+    }
+
+    IEnumerator DoMove() {
+        if(startDelay > 0f)
+            yield return new WaitForSeconds(startDelay);
+
+        var easeFunc = EaseManager.ToEaseFunction(tweenStyle);
+        var wait = new WaitForFixedUpdate();
+
+        while(mCurTime < delay) {
+            mCurTime += Time.fixedDeltaTime;
+            if(mCurTime > delay)
+                mCurTime = delay;
+
+            if(isPowered) {
+                float t = easeFunc(mCurTime, delay, 0f, 0f);
+                targetBody.MovePosition(Vector2.Lerp(mStartPosition, mEndPosition, t));
+            }
+            else {
+                float t = easeFunc(delay - mCurTime, delay, 0f, 0f);
+                targetBody.MovePosition(Vector2.Lerp(mEndPosition, mStartPosition, t));
+            }
+
+            yield return wait;
+        }
+
+        mRout = null;
+    }
+
+    private void OnDrawGizmos() {
+        if(targetBody) {
+            Vector2 startPos = targetBody.position;
+            Vector2 endPos = endIsLocal ? (Vector2)targetBody.transform.localToWorldMatrix.MultiplyPoint3x4(endPosition) : endPosition;
+
+            Gizmos.color = Color.green;
+
+            M8.Gizmo.ArrowLine2D(transform.position, startPos);
+
+            Gizmos.DrawLine(startPos, endPos);
+
+            Gizmos.color = Color.cyan * 0.5f;
+
+            Gizmos.DrawSphere(endPos, 0.25f);
+        }
+    }
+}
---
+++ Assets/Scripts/Forces/ConductiveReceiverBuoyancyFlowAngle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConductiveReceiverBuoyancyFlowAngle : ConductiveReceiver {
+    public BuoyancyEffector2D buoyancy;
+    public float toAngle;
+
+    private float mDefaultAngle;
+
+    void OnDisable() {
+        buoyancy.flowAngle = mDefaultAngle;
+    }
+
+    protected override void Awake() {
+        base.Awake();
+
+        mDefaultAngle = buoyancy.flowAngle;
+    }
+
+    protected override void OnPowerChanged() {
+        if(isPowered)
+            buoyancy.flowAngle = toAngle;
+        else
+            buoyancy.flowAngle = mDefaultAngle;
+    }
+}

bbfdb6f947e71123f8916f01cffdb3f80aef59b0
ddionisio <garapata@gmail.com>
2017-11-25 17:56:48 -0800

    restart ui art

--- Assets/Scripts/UI/Modals/ModalOptions.cs
+++ Assets/Scripts/UI/Modals/ModalOptions.cs
@@ -1,8 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ModalOptions : M8.UIModal.Controller, M8.UIModal.Interface.IPush, M8.UIModal.Interface.IPop {
+    public Slider musicSlider;
+    public Slider soundSlider;
 
     private bool mIsPaused;
 
@@ -13,6 +16,20 @@ public class ModalOptions : M8.UIModal.Controller, M8.UIModal.Interface.IPush, M
         UnhookInput();
     }
 
+    void Awake() {
+        musicSlider.onValueChanged.AddListener(OnMusicSliderValue);
+        soundSlider.onValueChanged.AddListener(OnSoundSliderValue);
+    }
+
+    public override void SetActive(bool aActive) {
+        base.SetActive(aActive);
+
+        if(aActive) {
+            musicSlider.normalizedValue = LoLManager.instance.musicVolume;
+            soundSlider.normalizedValue = LoLManager.instance.soundVolume;
+        }
+    }
+
     void M8.UIModal.Interface.IPush.Push(M8.GenericParams parms) {
         Pause(true);
 
@@ -20,6 +37,8 @@ public class ModalOptions : M8.UIModal.Controller, M8.UIModal.Interface.IPush, M
     }
 
     void M8.UIModal.Interface.IPop.Pop() {
+        LoLManager.instance.ApplyVolumes(soundSlider.normalizedValue, musicSlider.normalizedValue, true);
+
         Pause(false);
 
         UnhookInput();
@@ -30,6 +49,14 @@ public class ModalOptions : M8.UIModal.Controller, M8.UIModal.Interface.IPush, M
             Close();
     }
 
+    void OnMusicSliderValue(float val) {
+        LoLManager.instance.ApplyVolumes(soundSlider.normalizedValue, musicSlider.normalizedValue, false);
+    }
+
+    void OnSoundSliderValue(float val) {
+        LoLManager.instance.ApplyVolumes(soundSlider.normalizedValue, musicSlider.normalizedValue, false);
+    }
+    
     void UnhookInput() {
         if(M8.InputManager.instance)
             M8.InputManager.instance.RemoveButtonCall(OnInputEscape);

10eb8de2ee1c549e892f336fd3bf5b6c8f5647f7
ddionisio <garapata@gmail.com>
2017-12-09 18:57:46 -0800

    stage 2-3, stage 3-1 update, water update and art, stage 3 tile art

--- Assets/Scripts/Forces/Water.cs
+++ Assets/Scripts/Forces/Water.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class Water : MonoBehaviour {
     public const int triggerCacheCapacity = 8;
 
@@ -111,6 +112,9 @@ public class Water : MonoBehaviour {
 
     public string tagKillFilter = "Player";
 
+    public Transform fillRoot;
+    public Transform topRoot;
+
     private BuoyancyEffector2D mBuoyancy;
     private BoxCollider2D mColl;
 
@@ -158,6 +162,49 @@ public class Water : MonoBehaviour {
         }
     }
 
+#if UNITY_EDITOR
+    void Update() {
+        if(!Application.isPlaying) {
+            if(!mBuoyancy) mBuoyancy = GetComponent<BuoyancyEffector2D>();
+            if(!mColl) mColl = GetComponent<BoxCollider2D>();
+
+            if(!mColl || !mBuoyancy)
+                return;
+
+            var collBounds = mColl.bounds;
+
+            if(topRoot) {
+                Vector2 topPos = new Vector2(
+                    collBounds.center.x,
+                    transform.position.y + mBuoyancy.surfaceLevel);
+
+                topRoot.position = topPos;
+
+                Vector3 topScale = topRoot.localScale;
+                topScale.x = collBounds.size.x;
+
+                topRoot.localScale = topScale;
+            }
+
+            if(fillRoot) {
+                Vector2 fillPos = new Vector2(
+                    collBounds.center.x,
+                    transform.position.y);
+
+                fillRoot.position = fillPos;
+
+                Vector3 fillScale = fillRoot.localScale;
+                fillScale.x = collBounds.size.x;
+                fillScale.y = mBuoyancy.surfaceLevel;
+
+                fillRoot.localScale = fillScale;
+            }
+
+            return;
+        }
+    }
+#endif
+
     void FixedUpdate() {
         //check and update surfaces
         bool isNeedUpdate = false;
@@ -191,6 +238,11 @@ public class Water : MonoBehaviour {
     }
     
     void Awake() {
+#if UNITY_EDITOR
+        if(!Application.isPlaying)
+            return;
+#endif
+
         mBuoyancy = GetComponent<BuoyancyEffector2D>();
         mColl = GetComponent<BoxCollider2D>();
 
@@ -213,11 +265,24 @@ public class Water : MonoBehaviour {
 
         //recompute water surface level
         mBuoyancy.surfaceLevel = mSurfaceBaseLevel;
-
+                
         for(int i = 0; i < mSurfaceCache.Count; i++)
             mSurfaceCache[i].AddSurfaceLevel(this);
 
         if(mBuoyancy.surfaceLevel > mColl.size.y)
             mBuoyancy.surfaceLevel = mColl.size.y;
+
+        //apply top and fill telemetry
+        if(topRoot) {
+            Vector2 topPos = topRoot.localPosition;
+            topPos.y = mBuoyancy.surfaceLevel;
+            topRoot.transform.localPosition = topPos;
+        }
+
+        if(fillRoot) {
+            Vector3 fillScale = fillRoot.localScale;
+            fillScale.y = mBuoyancy.surfaceLevel;
+            fillRoot.localScale = fillScale;
+        }
     }
 }
